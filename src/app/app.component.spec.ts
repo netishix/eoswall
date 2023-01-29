5d4205232ba00680ac152ef572f026d8c863bcdd
Nahuel Vazquez <netishix@gmail.com>
2023-01-28 22:25:26 -0300

    fix(deploy): Fix settings for heroku deploy

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'eos-wall-update'`, () => {
+  it(`should have as title 'eos-wall'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('eos-wall-update');
+    expect(app.title).toEqual('eos-wall');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('eos-wall-update app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('eos-wall app is running!');
   });
 });
